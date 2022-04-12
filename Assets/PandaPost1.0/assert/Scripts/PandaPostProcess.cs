ca925f8be1ae31cfbb85c12b0af7887735e8c9ac
haolin <787067442@qq.com>
2022-04-12 16:27:54 +0800

    1

--- Assets/PandaPost1.0/assert/Scripts/PandaPostProcess.cs
+++ Assets/PandaPost1.0/assert/Scripts/PandaPostProcess.cs
@@ -13,8 +13,12 @@ public class PandaPostProcess : MonoBehaviour
 	public float Chromatic;
 	public float Frequency;
 	public float Amplitude;
-	public float VignettePower=1.5F;
+	public float VignettePower= 1.5F;
 	public float VignetteScale= 1.5F;
+	public float CenterU = 0.5F;
+	public float CenterV = 0.5F;
+
+
 	private void Awake()
 	{
 		if( PostProcessMat == null )
@@ -57,9 +61,19 @@ public class PandaPostProcess : MonoBehaviour
 		PostProcessMat.SetFloat("_zhenpinK", Frequency);
 
 		PostProcessMat.SetFloat("_zhenfuK", Amplitude);
+
 		PostProcessMat.SetFloat("_RedBlueFactorK", Chromatic);
+
 		PostProcessMat.SetFloat("_VignettePowerK", VignettePower);
+
 		PostProcessMat.SetFloat("_VignetteScaleK", VignetteScale);
+
+		PostProcessMat.SetFloat("_centerU", CenterU);
+
+		PostProcessMat.SetFloat("_centerV", CenterV);
+
+
+
 		Graphics.Blit( src, dest, PostProcessMat );
 	}
 }
